e0bcb941812d64c2d1a757ceddd65610ec33dce4
caiomelzer <melzer.caio@gmail.com>
2019-11-01 00:27:30 -0200

    Arquivos do projeto

---
+++ Scheduler/BizAgi_SIN5019_Scheduler_Service_Install.bat
@@ -0,0 +1,3 @@
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /name=BizAgiSIN5019SchedulerService BizAgi.Scheduler.Services.exe
+--NET START "BizAgiSIN5019SchedulerService"
+rem pause
---
+++ Scheduler/BizAgi_SIN5019_Scheduler_Service_UnInstall.bat
@@ -0,0 +1,3 @@
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /u /name=BizAgiSIN5019SchedulerService BizAgi.Scheduler.Services.exe
+sc delete BizAgiSIN5019SchedulerService
+rem pause
